75f4828b40d548ac0736a757c4e070349aad868a
jaredmulconry <jaredmulconry@gmail.com>
2016-01-03 18:48:30 +1100

    Added to clean.sh to more closely match the output of clean.bat

--- clean.sh
+++ clean.sh
@@ -5,16 +5,21 @@ func () {
 	local pathonly=""
 	find ./ -wholename '*/bin/DoNotDelete.txt' | while read path; do
 		pathonly=$(dirname "$path")
+		echo $pathonly
+		echo "folders:"
 		find $pathonly -maxdepth 1 -mindepth 1 -type d | while read folder; do
 			echo $folder
 			rm -rf $folder
 		done
+		echo
+		echo "files:"
 		find $pathonly -maxdepth 1 -mindepth 1 -type f | while read file; do
 			if [ "$(basename $file)" != "DoNotDelete.txt" ]; then
 				echo $file
 				rm $file
 			fi
 		done
+		echo
 	done
 }
 
